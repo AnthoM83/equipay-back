24f546fa278c7623aa973f22dce3285364dc1358
AnthoM83 <anthonymallada@gmail.com>
2023-10-31 21:31:45 -0300

    GastoDto y ConsultarDeudasDto devuelve nombre y apellido de los usuarios

--- src/main/java/com/proyecto/g2/equipay/services/DeudaService.java
+++ src/main/java/com/proyecto/g2/equipay/services/DeudaService.java
@@ -4,6 +4,9 @@ import com.proyecto.g2.equipay.commons.dtos.balance.BalanceDto;
 import com.proyecto.g2.equipay.commons.dtos.deuda.ConsultarDeudasDto;
 import com.proyecto.g2.equipay.commons.dtos.deuda.DeudaDto;
 import com.proyecto.g2.equipay.commons.dtos.deuda.Sugerencia;
+import com.proyecto.g2.equipay.commons.dtos.usuario.UsuarioDto;
+import com.proyecto.g2.equipay.models.Usuario;
+import com.proyecto.g2.equipay.repositories.IUsuarioRepository;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -16,10 +19,13 @@ public class DeudaService {
 
     @Autowired
     BalanceService balanceService;
+    @Autowired
+    IUsuarioRepository usuarioRepo;
 
     public ConsultarDeudasDto consultarDeudasDeUsuarioEnGrupo(Integer idGrupo, String idUsuario) {
+        Usuario usuario = usuarioRepo.findById(idUsuario).orElseThrow();
         ConsultarDeudasDto dto = new ConsultarDeudasDto();
-        dto.setIdUsuario(idUsuario);
+        dto.setUsuario(new UsuarioDto(usuario.getCorreo(), usuario.getNombre(), usuario.getApellido()));
         dto.setIdGrupo(idGrupo);
 
         List<BalanceDto> deudasConsultado = balanceService.listarBalancesDeUsuarioEnGrupo(idUsuario, idGrupo);
@@ -44,6 +50,7 @@ public class DeudaService {
             Double restanteAPagar = deudaConsultado.getDeuda();
             deuda.setDeudaEnGrupo(Precision.round(restanteAPagar, 2));
             for (BalanceDto otro : aFavorOtros) {
+                Usuario otroUsuario = usuarioRepo.findById(otro.getIdUsuario()).orElseThrow();
                 Double pagoEnEstaIteracion;
                 if (otro.getMoneda().equals(moneda) && restanteAPagar != 0.0) {
                     Double otroAFavorAbs = Math.abs(otro.getDeuda());
@@ -54,7 +61,7 @@ public class DeudaService {
                         pagoEnEstaIteracion = restanteAPagar;
                         restanteAPagar = 0.0;
                     }
-                    Sugerencia sugerencia = new Sugerencia(otro.getIdUsuario(), pagoEnEstaIteracion);
+                    Sugerencia sugerencia = new Sugerencia(new UsuarioDto(otroUsuario.getCorreo(), otroUsuario.getNombre(), otroUsuario.getApellido()), pagoEnEstaIteracion);
                     deuda.getSugerencias().add(sugerencia);
                 }
             }
