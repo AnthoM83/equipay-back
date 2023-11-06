0fe327a2a7a18bc5f591c23b303b4e0e51550a8f
Sofia Guerra <sofiguerra27@hotmail.com>
2023-11-06 01:47:24 -0300

    add test

--- src/main/java/com/proyecto/g2/equipay/models/EmailRequest.java
+++ src/main/java/com/proyecto/g2/equipay/models/EmailRequest.java
@@ -1,10 +1,12 @@
 package com.proyecto.g2.equipay.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class EmailRequest {
     private String destinatario;
     private String asunto;
