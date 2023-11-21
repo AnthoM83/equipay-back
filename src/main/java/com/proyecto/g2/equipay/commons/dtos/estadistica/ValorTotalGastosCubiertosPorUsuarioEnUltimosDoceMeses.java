e2c4e9ae8449cac89686fbe2528360b0b1342012
mrestre95 <mauroc2006@hotmail.com>
2023-11-21 19:14:20 -0300

    Arreglos en resumenes

--- src/main/java/com/proyecto/g2/equipay/commons/dtos/estadistica/ValorTotalGastosCubiertosPorUsuarioEnUltimosDoceMeses.java
+++ src/main/java/com/proyecto/g2/equipay/commons/dtos/estadistica/ValorTotalGastosCubiertosPorUsuarioEnUltimosDoceMeses.java
@@ -1,6 +1,5 @@
 package com.proyecto.g2.equipay.commons.dtos.estadistica;
 
-import java.time.YearMonth;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +7,14 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ValorTotalGastosCubiertosPorUsuarioEnUltimosDoceMeses {
+public class ValorTotalGastosCubiertosPorUsuarioEnUltimosDoceMeses implements Comparable<ValorTotalGastosCubiertosPorUsuarioEnUltimosDoceMeses> {
 
-    private YearMonth mes;
+    private String fechaCompleta;
+    private String mesAbreviado;
     private Double valor;
+
+    @Override
+    public int compareTo(ValorTotalGastosCubiertosPorUsuarioEnUltimosDoceMeses otro) {
+        return this.fechaCompleta.compareTo(otro.fechaCompleta);
+    }
 }
